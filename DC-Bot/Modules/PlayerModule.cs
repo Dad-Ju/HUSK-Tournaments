d7eaa44fc0e430af2196302526c9ec65d47e4e48
Dad-Ju <Justin.Bedrich@gmail.com>
2024-11-21 01:13:45 +0100

    Update the Database Schema and add Seeding Data

--- DC-Bot/Modules/PlayerModule.cs
+++ DC-Bot/Modules/PlayerModule.cs
@@ -6,11 +6,8 @@ namespace DC_Bot.Modules;
 
 public class PlayerModule : InteractionModuleBase<SocketInteractionContext>
 {
-    // Dependencies can be accessed through Property injection, public properties with public setters will be set by the service provider
-    public InteractionService Commands { get; set; }
-
+    private readonly DatabaseContext db;
     private InteractionHandler _handler;
-    private DatabaseContext db;
 
     // Constructor injection is also a valid way to access the dependencies
     public PlayerModule(InteractionHandler handler, DatabaseContext database)
@@ -19,14 +16,17 @@ public class PlayerModule : InteractionModuleBase<SocketInteractionContext>
         db = database;
     }
 
-    [SlashCommand("debug", description: "Debug Player DB")]
+    // Dependencies can be accessed through Property injection, public properties with public setters will be set by the service provider
+    public InteractionService Commands { get; set; }
+
+    [SlashCommand("debug", "Debug Player DB")]
     public async Task DebugPlayerDB()
     {
         db.Players.Add(
-            new Player { PlayerID = "audhadiada", Description = "Im Juu", discordId = "269217065896771584" });
+            new Player { Description = "Im Juu", DiscordId = Context.User.Id.ToString() });
         await db.SaveChangesAsync();
 
-        await RespondAsync(text: "Debug Player DB");
+        await RespondAsync("Debug Player DB");
     }
 
     [Group("profile", "Everything about you and other players")]
@@ -34,19 +34,27 @@ public class PlayerModule : InteractionModuleBase<SocketInteractionContext>
     {
         [SlashCommand("me", "Shows everything about your yourself!")]
         public async Task PlayerSelf()
-            => await RespondAsync("Something for now!");
+        {
+            await RespondAsync("Something for now!");
+        }
 
         [SlashCommand("register", "Allows you to register!")]
         public async Task PlayerNew()
-            => await RespondAsync("Something for now, Register!");
+        {
+            await RespondAsync("Something for now, Register!");
+        }
 
         [SlashCommand("lookup", "Shows everything about other players!")]
         public async Task PlayerSearch()
-            => await RespondAsync("Something for now, Search!");
+        {
+            await RespondAsync("Something for now, Search!");
+        }
 
         [SlashCommand("inbox", "Shows your Inbox and Invites!")]
         public async Task PlayerInbox()
-            => await RespondAsync("Something for now, Inbox!");
+        {
+            await RespondAsync("Something for now, Inbox!");
+        }
     }
 
     [Group("teams", "Everything about you and other players")]
@@ -54,18 +62,26 @@ public class PlayerModule : InteractionModuleBase<SocketInteractionContext>
     {
         [SlashCommand("profile", "Allows you to register!")]
         public async Task TeamProfile()
-            => await RespondAsync("Something for now, Register!");
+        {
+            await RespondAsync("Something for now, Register!");
+        }
 
         [SlashCommand("lookup", "Shows everything about your yourself!")]
         public async Task TeamLookup()
-            => await RespondAsync("Something for now!");
+        {
+            await RespondAsync("Something for now!");
+        }
 
         [SlashCommand("create", "Shows your Inbox and Invites!")]
         public async Task PlayerInbox()
-            => await RespondAsync("Something for now, Inbox!");
+        {
+            await RespondAsync("Something for now, Inbox!");
+        }
 
         [SlashCommand("leave", "Shows everything about other players!")]
         public async Task PlayerSearch()
-            => await RespondAsync("Something for now, Search!");
+        {
+            await RespondAsync("Something for now, Search!");
+        }
     }
 }
--- Database/DatabaseContext.cs
+++ Database/DatabaseContext.cs
@@ -1,31 +1,39 @@
-﻿using Database.Models;
+﻿using System.Reflection;
+using Database.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace Database;
 
 public class DatabaseContext : DbContext
 {
+    public DatabaseContext()
+    {
+        var folder = Environment.SpecialFolder.ApplicationData;
+        var path = Path.Combine(Environment.GetFolderPath(folder), "HUSK-Tournaments");
+        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+        DbPath = Path.Join(path, "tournament.db");
+    }
+
+    private string DbPath { get; }
+
     public DbSet<Player> Players { get; set; }
     public DbSet<PlayerAccount> PlayerAccounts { get; set; }
-    public DbSet<Invite> Invites { get; set; }
     public DbSet<Team> Teams { get; set; }
+    public DbSet<TeamRole> TeamRoles { get; set; }
+    public DbSet<PlayerTeamRole> PlayerTeamRoles { get; set; }
+    public DbSet<Invite> Invites { get; set; }
+    public DbSet<Match> Matches { get; set; }
 
-    private IConfiguration? _configuration;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        _configuration = new ConfigurationBuilder()
-            .AddEnvironmentVariables(prefix: "DB_")
-            // ReSharper disable once StringLiteralTypo
-            .AddJsonFile("appsettings.json", optional: true)
-            .Build();
+        optionsBuilder.UseSqlite(@$"Data Source={DbPath}");
+    }
 
-        if (string.IsNullOrWhiteSpace(_configuration["connectionString"]))
-        {
-            throw new NullReferenceException("ConnectionString is missing");
-        }
 
-        optionsBuilder.UseSqlite(@$"Data Source={_configuration["connectionString"]}");
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
